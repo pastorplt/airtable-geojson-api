982494530ee2a098513c9e5f009e76f7c918628c
pastorplt <paul@tbc.city>
2025-08-06 20:30:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.get("/", async (req, res) => {
           type: "Feature",
           geometry,
           properties: {
-            name: r.fields["Name"] || "",
-            network: r.fields["Network"] || ""
+            name: r.fields["Network Name"] || "",
+            network: r.fields["Network Leaders"] || ""
           }
         };
       } catch {
